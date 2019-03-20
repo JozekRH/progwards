32493a16a952aad05269e192f4ddbdcce5cba6a7
JozekRH <kolybaev@gmail.com>
2019-03-20 02:00:08 +0200

    Test1

--- Test1/TestTask21.java
+++ Test1/TestTask21.java
@@ -2,12 +2,12 @@ package Test1;
 
 public class TestTask21 {
 	
-	public String t111() {
+	public String t1() {
 		//int[] a = null; a[0] = 0;
 		return "111";
 	}
 
-	public String t2() {
-		return "222";
+	public int t2() {
+		return 222;
 	}
 }
