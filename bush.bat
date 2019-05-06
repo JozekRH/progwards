b1cb1436094728f589f929fa08dd4e232774ca71
Progwards <hwtest@progwards.ru>
2019-05-06 16:58:03 +0200

    Test1

--- bush.bat
+++
@@ -1,4 +0,0 @@
-git init
-git add .
-git commit -m "Test1"
-git push -u origin master
---
+++ ru/progwards/java1/SeaBattle/admin/BattleAlg.java
@@ -0,0 +1,8 @@
+package ru.progwards.java1.SeaBattle.admin;
+
+public class BattleAlg extends ru.progwards.java1.SeaBattle.BattleAlg {
+	@Override
+	public String test(String n) {
+		return n+n;
+	}
+}
