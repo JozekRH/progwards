5d7759ec2dc2056941a37dff6370373e3838087e
Progwards <hwtest@progwards.ru>
2019-05-19 00:40:12 +0200

    Test1

--- ru/progwards/java1/SeaBattle/admin/BattleAlg.java
+++ ru/progwards/java1/SeaBattle/admin/BattleAlg.java
@@ -1,7 +1,6 @@
 package ru.progwards.java1.SeaBattle.admin;
 
-public class BattleAlg extends ru.progwards.java1.SeaBattle.BattleAlg {
-	@Override
+public class BattleAlg {
 	public String test(String n) {
 		return n+n;
 	}
