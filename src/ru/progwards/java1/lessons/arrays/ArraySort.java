36c2d4e1c2cbb720ed34c255767178c453f4a179
JozekRH <47397991+JozekRH@users.noreply.github.com>
2019-08-06 02:12:04 +0300

    Add files via upload

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.arrays;
 
 public class ArraySort {
 
-	public static int[] sort(int[] a)
+	public static void sort(int[] a)
 	{
 		for (int i = 0; i < a.length; i++)
 		{
@@ -16,7 +16,5 @@ public class ArraySort {
 				}
 			}
 		}
-		
-		return a;
 	}
 }
