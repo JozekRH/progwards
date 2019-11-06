5ce6f00cc864f246b65c0f2a8dbf03aae06a4cce
JozekRH <47397991+JozekRH@users.noreply.github.com>
2019-11-06 15:47:43 +0200

    Add files via upload

---
+++ src/Lip/progwards/ev/HelloWorld.java
@@ -0,0 +1,11 @@
+package Lip.progwards.ev;
+
+public class HelloWorld {
+    static int addition(int x, int y){
+        return x+y;
+    }
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -0,0 +1,23 @@
+package ru.progwards.java1.lessons.helloworld;
+
+public class Task1 {
+    public static void main(String[] args){
+        String str1="Хорошо идут дела";
+        String str2="Изучаю Java я!";
+        String str3=" ";
+        //Хорошо идут дела
+        System.out.println(str1);
+        //Изучаю Java я!
+        System.out.println(str2);
+        //Хорошо идут дела Изучаю Java я!
+        System.out.print(str1);
+        System.out.print(str3);
+        System.out.println(str2);
+        //Изучаю Java я! Хорошо идут дела
+        System.out.print(str2);
+        System.out.print(str3);
+        System.out.print(str1);
+
+    }
+
+}
