35e2cad862e91be8213efe972aedad9123e4210e
JozekRH <47397991+JozekRH@users.noreply.github.com>
2020-02-29 16:04:41 +0200

    Add files via upload

---
+++ src/ru/progwards/java1/lessons/recursion/Calculate.java
@@ -0,0 +1,11 @@
+package ru.progwards.java1.lessons.queues;
+
+public class Calculate {
+    public static double calculation1(){
+        return 2.2 * (3 + 12.1);
+    }
+
+    public static double calculation2(){
+        return (737.22 + 24) / (55.6 - 12.1) + (19 - 3.33)*(87 + 2*(13.001 - 9.2));
+    }
+}
