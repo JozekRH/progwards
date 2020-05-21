50d4a5f05d89827a53696910167a4fdd4cdb3936
JozekRH <47397991+JozekRH@users.noreply.github.com>
2020-05-21 15:59:52 +0300

    Update TriangleSimpleInfo.java

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -1,39 +1,66 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
 
-public class TriangleSimpleInfo {
-    public static int maxSide(int a, int b, int c) {
-        int max = 0;
-        if (a > b)
-            max = a;
+public class TriangleSimpleInfo<a> {
+    /*
+  //--------------------------------------------------
+  //
+  */
+    /*1.1 Реализовать функцию
+    public static int maxSide(int a, int b, int c),
+    которая получает параметрами длины сторон треугольника,
+    а вернуть должна наибольшую длину стороны. */
+    public static int maxSide(int a,int b,int c) {
+        int inputArray[] = {a, b, c};
+        //int instValue;
+        int maxValue = inputArray[0];
+        for (int i = 1; i < inputArray.length; i++)
+        {    //instValue = inputArray[i];
+            if (inputArray[i] > maxValue)
+            {
+                maxValue = inputArray[i];
+            }
+        }
+        return maxValue;
+    }
+    /*1.2 Реализовать функцию
+    public static int minSide(int a, int b, int c),
+    которая получает параметрами длины сторон треугольника,
+    а вернуть должна наименьшую длину стороны.*/
+    public static int minSide(int a, int b, int c)
+    {
+        int inputArray[] = {a, b, c};
+        //int instValue;
+        int minValue = inputArray[0];
+        for (int i = 1; i < inputArray.length; i++)
+        {    //instValue = inputArray[i];
+            if (inputArray[i] < minValue)
+            {
+                minValue = inputArray[i];
+            }
+        }
+        return minValue;
+    }
+    /*1.3 Реализовать функцию
+    public static boolean isEquilateralTriangle(int a, int b, int c),
+    которая получает параметрами длины сторон треугольника,
+    а вернуть должна true, если треугольник равносторонний и
+   false в противном случае.*/
+    public static boolean isEquilateralTriangle(int a, int b, int c)
+    {
+        boolean equal;
+        if ((a==b)&(b==c)){ equal=true;}
         else
-            max = b;
-        if (max < c)
-            max = c;
-
-        return max;
+            {
+              equal=false;
+            }
+        return equal;
     }
 
-    public static int minSide(int a, int b, int c) {
-        int min = 0;
-        if (a < b)
-            min = a;
-        else
-            min = b;
 
-        if (min > c)
-            min = c;
-
-        return min;
-    }
-
-    public static boolean isEquilateralTriangle(int a, int b, int c) {
-        return a == b && a == c;
-    }
 
     public static void main(String[] args) {
-        int a = 3, b = 4, c = 5;
-        System.out.println(maxSide(a, b, c));
-        System.out.println(minSide(a, b, c));
-        System.out.println(isEquilateralTriangle(a, b, c));
+        System.out.println("Maximum Value is: "+ maxSide(2,6,3));
+        System.out.println("minimum Value is: "+ minSide(5,7,4));
+        System.out.println("equal Value is: "+ isEquilateralTriangle(5,5,5));
     }
 }
