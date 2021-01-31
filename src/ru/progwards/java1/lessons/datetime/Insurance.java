68d60825ef9acd26c033ff051229fa7efaace76f
JozekRH <47397991+JozekRH@users.noreply.github.com>
2021-01-31 20:51:30 +0200

    Add files via upload

--- src/ru/progwards/java1/lessons/datetime/Insurance.java
+++ src/ru/progwards/java1/lessons/datetime/Insurance.java
@@ -1,77 +1,160 @@
 package ru.progwards.java1.lessons.datetime;
-//123123123
-//2222
+/* Класс должен проверять валидность страховок, например для выезжающих за рубеж.
+Каждая страховка имеет дату-время начала, и продолжительность.*/
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
+
+import java.time.*;
 
 public class Insurance {
-    //lalala
+    public static void main(String[] args) {
+/*//        String str = "01.01.2020 16:00:00.000 +0300 Moscow Standard Time";
+        String str = "2020-01-01T16:00+03:00[Europe/Moscow]"; //2020-01-01T16:00+03:00[Europe/Moscow]
+        Locale locale = Locale.US;
+//        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss.SSS Z zzzz").withLocale(locale);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_ZONED_DATE_TIME;
+        ZonedDateTime zdt_test1 = ZonedDateTime.parse("2020-01-01T16:00+03:00[Europe/Moscow]", dateTimeFormatter);
 
-    public static enum FormatStyle {SHORT, LONG, FULL};
-    private ZonedDateTime start;
-    private Duration duration;
-    
-    public Insurance(ZonedDateTime start){
+//        ZonedDateTime zdt_test3 = ZonedDateTime.parse("2020-12-01T16:00+03:00[Europe/Moscow]", dateTimeFormatter);
+//        ZonedDateTime zdt_test4 = ZonedDateTime.parse("2021-11-01T16:00+03:00[Europe/Moscow]", dateTimeFormatter);
+//        ZonedDateTime zdt_test5 = zdt_test3.plusDays(366);
+//        System.out.println(zdt_test3.compareTo(zdt_test4));
+//        System.out.println(zdt_test1);
+//        System.out.println(zdt_test2);
+//        System.out.println(zdt_test3 + "\n");
+
+//        Duration duration1 = Duration.ofDays(32);   //Установка продолжительности через дни
+////        System.out.println(duration1.toString());   //PT1440H
+//        //Использование первого конструктора
+//        Insurance ins_1 = new Insurance(zdt_test1);
+////        ins_1.setDuration(duration1);               //Продолжительность 1
+//        ZonedDateTime zdt_test6 = ZonedDateTime.parse("2020-02-01T16:00+03:00[Europe/Moscow]", dateTimeFormatter);
+////        System.out.println(ins_1.checkValid(zdt_test6));
+//        System.out.println(ins_1.toString());
+//        //Использование второго конструктора
+//        String str2 = "2020-09-01";
+//        Insurance ins_2 = new Insurance(str2, FormatStyle.SHORT);
+//        ZonedDateTime zdt_test2 = ZonedDateTime.parse("2021-09-01T00:00+03:00[Europe/Moscow]", dateTimeFormatter);
+//        ins_2.setDuration(zdt_test2);
+//        System.out.println(ins_2.toString());
+//
+//        String str3 = "2020-10-01T02:00";
+//        Insurance ins_3 = new Insurance(str3, FormatStyle.LONG);
+//        ins_3.setDuration( 6, 1, 1);
+//        System.out.println(ins_3.toString());
+//
+//        String str4 = "2020-05-01T16:00+03:00[Europe/Moscow]";
+//        Insurance ins_4 = new Insurance(str4, FormatStyle.FULL);
+//        ins_4.setDuration("2592000000", FormatStyle.SHORT); //Продолжительность - месяц
+//        System.out.println(ins_4.toString());
+
+        String str5 = "2020-12-20T00:00";
+        Insurance ins_5 = new Insurance(str5, FormatStyle.LONG);
+        ins_5.setDuration("0000-01-04T06:10", FormatStyle.LONG); //Продолжительность - 2 месяца
+        System.out.println(ins_5.toString());
+
+//        String str6 = "2021-01-01T00:00";
+//        Insurance ins_6 = new Insurance(str6, FormatStyle.LONG);
+//        ins_6.setDuration("PT1440H", FormatStyle.FULL); //Продолжительность - 2 месяца
+//        System.out.println(ins_6.toString());*/
+
+    }
+
+//  1.1  Реализовать локальный public static enum FormatStyle {SHORT, LONG, FULL} - стиль формата даты-времени
+    public static enum FormatStyle {SHORT, LONG, FULL}
+
+//  1.2. Реализовать приватные свойства класса:
+    private ZonedDateTime start; //- дата-время начала действия страховки.
+    private Duration duration; //- продолжительность действия.
+
+//  1.2 Реализовать конструкторы:
+//  - установить дату-время начала действия страховки.
+    public Insurance(ZonedDateTime start) throws DateTimeException{
         this.start = start;
     }
-    
-    public Insurance(String strStart, FormatStyle style){
+
+//  - установить дату-время начала действия страховки
+/*    SHORT соответствует ISO_LOCAL_DATE
+    LONG  - ISO_LOCAL_DATE_TIME
+    FULL - ISO_ZONED_DATE_TIME
+    Для вариантов, когда не задан явно часовой пояс использовать таковой по умолчанию.*/
+    public Insurance(String strStart, FormatStyle style) throws DateTimeException{
         switch (style){
             case SHORT:
-                start = ZonedDateTime.of(LocalDate.parse(strStart, DateTimeFormatter.ISO_LOCAL_DATE), LocalTime.MIDNIGHT, ZoneId.systemDefault());
+                LocalDate localDate = LocalDate.parse(strStart);    //формат ISO_LOCAL_DATE
+                this.start = ZonedDateTime.of(localDate, LocalTime.MIDNIGHT, ZoneId.of("Europe/Moscow"));   //LocalTime.MIDNIGHT принял
                 break;
             case LONG:
-                start = ZonedDateTime.parse(strStart, DateTimeFormatter.ISO_LOCAL_DATE_TIME.withZone(ZoneId.systemDefault()));
+                LocalDateTime localDateTime = LocalDateTime.parse(strStart);    //Формат ISO_LOCAL_DATE_TIME
+                this.start = ZonedDateTime.of(localDateTime, ZoneId.of("Europe/Moscow"));
                 break;
             case FULL:
-                start = ZonedDateTime.parse(strStart, DateTimeFormatter.ISO_ZONED_DATE_TIME);
+                this.start = ZonedDateTime.parse(strStart);     //Формат ISO_ZONED_DATE_TIME
                 break;
         }
-        
     }
-    
+
+//  1.3 Реализовать методы, устанавливающие продолжительность действия страховки:
+//  - установить продолжительность действия страховки.
     public void setDuration(Duration duration){
         this.duration = duration;
     }
-    
+
+//  - установить продолжительность действия страховки, задав дату-время окончания.
     public void setDuration(ZonedDateTime expiration){
         this.duration = Duration.between(start, expiration);
     }
-    
+
+//  - установить продолжительность действия страховки, задав целыми числами количество месяцев, дней и часов.
     public void setDuration(int months, int days, int hours){
-        setDuration(ZonedDateTime.from(start).plusMonths(months).plusDays(days).plusHours(hours));
+        this.duration = Duration.between(start, start.plusMonths(months).plusDays(days).plusHours(hours));
     }
-    
-    public void setDuration(String strDuration, FormatStyle style){
+
+//  - установить продолжительность действия страховки
+  /*SHORT - целое число миллисекунд (тип long)
+    LONG  - ISO_LOCAL_DATE_TIME - как период, например “0000-06-03T10:00:00” означает,
+            что продолжительность действия страховки 0 лет, 6 месяцев, 3 дня 10 часов.
+            FULL - стандартный формат Duration, который получается через toString()*/
+    public void setDuration(String strDuration, FormatStyle style) throws RuntimeException{
         switch (style){
-        case SHORT:
-            setDuration(ZonedDateTime.from(start).plus(Long.parseLong(strDuration), ChronoUnit.MILLIS));
-            break;
-        case LONG:
-            ZonedDateTime zdt = ZonedDateTime.parse(strDuration, DateTimeFormatter.ISO_LOCAL_DATE_TIME.withZone(ZoneId.systemDefault()));
-            setDuration(ZonedDateTime.from(start).plusYears(zdt.getYear())
-                    .plusMonths(zdt.getMonthValue()).plusDays(zdt.getDayOfMonth())
-                    .plusHours(zdt.getHour()).plusMinutes(zdt.getMinute()).plusSeconds(zdt.getSecond()));
-            break;
-        case FULL:
-            setDuration(Duration.parse(strDuration));
-            break;
+            case SHORT:
+                this.duration = Duration.ofMillis(Long.parseLong(strDuration));
+                break;
+            case LONG:
+                LocalDateTime localDateTime = LocalDateTime.parse(strDuration);  //ISO_LOCAL_DATE_TIME;
+                this.duration = Duration.between(start, start.plusYears(localDateTime.getYear())
+                                                        .plusMonths(localDateTime.getMonthValue())
+                                                        .plusDays(localDateTime.getDayOfYear())
+                                                        .plusHours(localDateTime.getHour()));
+                break;
+            case FULL:
+                this.duration = Duration.parse(strDuration);
+                break;
         }
-        
     }
-    
-    public boolean checkValid(ZonedDateTime dateTime){
-        return dateTime.isAfter(ZonedDateTime.from(start)) && ((duration == null) ? true : dateTime.isBefore(ZonedDateTime.from(start).plus(duration)));
+
+//  1.4 Реализовать методы возврата информации:
+//  - проверить действительна ли страховка на указанную дату-время.
+// Если продолжительность не задана считать страховку бессрочной.
+    public boolean checkValid(ZonedDateTime dateTime) {
+        if(dateTime.compareTo(this.start) >=0){                 //Если текущая дата позднее начала действия страховки
+            if(this.duration != null){                          //Если продолжительность установлена
+                return dateTime.compareTo(this.start.plusDays(duration.toDays())) < 0;
+            }else return true;
+        }else return false;
     }
-    
-    public String toString(){
-        ZonedDateTime zdt = ZonedDateTime.now();
-        return "Insurance issued on " + start + (checkValid(zdt) ? " is valid" : " is not valid");
+
+//  - вернуть строку формата "Insurance issued on " + start + validStr,
+//  где validStr = " is valid", если страховка действительна на данный момент и
+//  " is not valid", если она недействительна.
+    public String toString() {
+        String validStr;
+        ZonedDateTime thisDay = Instant.now().atZone(ZoneId.of("Europe/Moscow"));       //Определение текущей даты
+        if(checkValid(thisDay)){
+            validStr = " is valid";
+        }else validStr = " is not valid";
+        return "Insurance issued on " + this.start + validStr;
+//        return "Insurance issued on " + this.start + " Продолжительность действия " + this.duration.toDays();
     }
+
+
 }
--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -1,70 +1,130 @@
 package ru.progwards.java1.lessons.datetime;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.time.Instant;
+import java.util.*;
+
+/*Реализовать класс для ручной профилировки производительности программного кода.
+Профилировка программного кода - измерение времени выполнения
+отдельных фрагментов кода с целью выявления узких мест в производительности.
+
+Секции могут быть только вложенные, как матрешки, и при этом поддержку рекурсии реализовывать не нужно.
+Но каждая секция может встретиться несколько раз.*/
 
 public class Profiler {
-    
-    private static Map<String, StatisticInfo> statisticInfos = new TreeMap<>();
-    private static Map<String, Long> openSectionsStartTimes = new TreeMap<>();
-    private static Map<String, Set<String>> innerSections = new TreeMap<>();
-    
-    public static void enterSection(String name){
-        
-        System.out.println("Вход в сессию " + name);
-        for (String s: openSectionsStartTimes.keySet()){
-            if (innerSections.get(s) != null) innerSections.get(s).add(name);
-            else{
-                Set<String> is = new HashSet<String>();
-                is.add(name);
-                innerSections.put(s, is);
+    public static void main(String[] args) {
+        int s1 = 0;
+        int s2 = 0;
+        int s3 = 0;
+
+        Profiler profiler = new Profiler();
+        StatisticInfo profStatInf = new StatisticInfo();
+
+        long start = System.currentTimeMillis();
+
+        profiler.enterSection("Section 1");
+        for (int i = 0; i < 100000; i++){
+            s1++;
+            profiler.enterSection("Section 2");
+            for (int a = 0; a < 100; a++){
+                s2++;
+                profiler.enterSection("Section 3");
+                for (int b = 0; b < 10; b++){
+                    s3++;
+                }
+                exitSection("Section 3");
             }
+            exitSection("Section 2");
         }
+        exitSection("Section 1");
+        System.out.println("Полное время " + (System.currentTimeMillis() - start) + " мсек");
 
-        openSectionsStartTimes.put(name, new Date().getTime());
+        for (StatisticInfo stInf:getStatisticInfo())
+        System.out.println(stInf);
     }
-    
+
+
+    //Переменные класса
+    public static Map<String, StatisticInfo> statisticInfoMap = new HashMap<>();    //Хранение информации о секциях с общим доступом
+    public static Map<Integer, String> depthMap = new HashMap<>();  //Дополниетльное хранилище для определения вложенности секций
+
+    //2.1 Реализовать методы
+    //- войти в профилировочную секцию, замерить время входа
+    //TODO:Попробовать сделать через стек
+    public static void enterSection(String name){
+        if(statisticInfoMap.containsKey(name)){
+            statisticInfoMap.get(name).count += 1;
+            statisticInfoMap.get(name).sectionTime = (int)System.currentTimeMillis();   //Запись времени входа в секцию
+        }else {
+            StatisticInfo profStatInf = new StatisticInfo();        //Создание секции
+            profStatInf.sectionName = name;
+            profStatInf.sectionTime = (int)System.currentTimeMillis();
+            profStatInf.depthSection = depthMap.size();             //Установка и запись глубины вложенности
+            statisticInfoMap.put(name, profStatInf);
+            depthMap.put(depthMap.size(), name);
+        }
+    }
+
+    //2.2 выйти из профилировочной секции.
+    // Замерить время выхода, вычислить промежуток времени между входом и выходом в миллисекундах.
     public static void exitSection(String name){
-        StatisticInfo si = statisticInfos.get(name);
-        if (si == null){
-            si = new StatisticInfo();
-            si.sectionName = name;
-            si.fullTime = 0;
-            si.selfTime = 0;
-            si.count = 0;
+        if(statisticInfoMap.containsKey(name)){
+            int i = (int)System.currentTimeMillis() - statisticInfoMap.get(name).sectionTime;   //Определение времени прохождения секции
+            statisticInfoMap.get(name).fullTime += i;                       //перезапись значения суммарного времени
+            statisticInfoMap.get(name).selfTime = selfTimeCalc(name);       //вычисление чистого времени
         }
-        si.fullTime += new Date().getTime() - openSectionsStartTimes.get(name);
-        si.selfTime = si.fullTime - calcInnerTimes(name);
-        si.count++;
+    }
 
-        openSectionsStartTimes.remove(name);
-        innerSections.remove(name);
-        statisticInfos.put(name, si);
-        
-        System.out.println("Выход из сессии " + name);
-    }
-    
-    public static List<StatisticInfo> getStatisticInfo(){
-        List<StatisticInfo> result = new ArrayList<>();
-        result.addAll(statisticInfos.values());
-        return result;
-    }
-    
-    private static int calcInnerTimes(String name){
-        int result = 0;
-        if (innerSections.get(name) == null) return result;
-        
-        for (String s : innerSections.get(name)){
-            result += statisticInfos.get(s).selfTime;
+    //Метод для вычисления чистого времени секции
+    public static int selfTimeCalc(String name){
+        if(statisticInfoMap.get(name).depthSection != depthMap.size() - 1){     //Если секция не последняя
+            String upperName = depthMap.get(statisticInfoMap.get(name).depthSection + 1); //Получаем имя секции на уровень ниже, вложенной
+            return statisticInfoMap.get(name).fullTime - statisticInfoMap.get(upperName).fullTime;
+        }else{
+            return statisticInfoMap.get(name).fullTime;     //Если последняя просто записываем одинаковое время
         }
-        return result;
     }
-    
-    
+
+    //2.3 получить профилировочную статистику, отсортировать по наименованию секции
+     public static List<StatisticInfo> getStatisticInfo(){
+         List<StatisticInfo> resultList = new ArrayList<>();
+         for(var value : statisticInfoMap.entrySet()){          //Собираю список из объектов секций
+            resultList.add(value.getValue());
+         }
+        resultList.sort(new Comparator<StatisticInfo>() {           //Сортировка
+            @Override
+            public int compare(StatisticInfo o1, StatisticInfo o2) {
+                return o1.sectionName.compareTo(o2.sectionName);
+            }
+        });
+        return resultList;
+     }
+
+    //2.4 Реализовать class StatisticInfo
+/*    class StatisticInfo{
+        //Переменные класса
+        public String sectionName;      //имя секции
+        //Полное время выполнения секции в миллисекундах.
+        public int fullTime = 0;
+        //чистое время выполнения секции в миллисекундах.
+        // Для вложенных секций, из времени выполнения внешней секции нужно вычесть времена выполнения вложенных секций.
+        public int selfTime = 0;
+        //количество вызовов. В случае, если вызовов более одного,
+        // fullTime и selfTime содержат суммарное время выполнения всех вызовов.
+        public int count = 1;
+
+        public int sectionTime;     //Время выполнения секции
+        public int depthSection; //Внешняя секция
+
+        @Override
+        public String toString() {
+            return "StatisticInfo{" +
+                    "sectionName='" + sectionName + '\'' +
+                    ", fullTime=" + fullTime +
+                    ", selfTime=" + selfTime +
+                    ", count=" + count +
+                    '}';
+        }
+    }*/
+
 
 }
