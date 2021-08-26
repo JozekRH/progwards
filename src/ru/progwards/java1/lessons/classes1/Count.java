48c5545babeb1f97a5d270471c199520c8b07cd1
JozekRH <47397991+JozekRH@users.noreply.github.com>
2021-08-26 23:45:03 +0200

    Add files via upload

---
+++ src/ru/progwards/java1/lessons/classes1/Count.java
@@ -0,0 +1,34 @@
+package ru.progwards.java1.lessons.classes1;
+
+public class Count {
+
+    private int count;
+
+    public Count(){
+        this.count = 0;
+    }
+
+    public Count(int count){
+        this.count = count;
+    }
+
+    public int getCount(){
+        return this.count;
+    }
+
+    public void inc(){
+        this.count++;
+    }
+
+    public boolean dec(){
+        this.count--;
+        if (this.count <= 0) return true;
+        else return false;
+    }
+
+    public static void main(String[] args){
+        Count count = new Count(10);
+        while (count.dec() == false){}
+        System.out.println("count равен 0");
+    }
+}
